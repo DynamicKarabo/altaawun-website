5c7d1cbe40c0794703077f8af584d83ac9a51645
Karabo Oliphant🧬 <karabooliphant34@gmail.com>
2025-11-24 13:41:29 +0200

    Update Partners.tsx changed Shoprite logo

--- ALTAAWUNredesign1.1 (Community) (16)/src/components/Partners.tsx
+++ ALTAAWUNredesign1.1 (Community) (16)/src/components/Partners.tsx
@@ -7,7 +7,7 @@ export function Partners() {
     { name: 'Proudly South Africa', image: 'https://i.postimg.cc/7hdMNtFn/pngegg.png' },
     { name: 'PPC', image: 'https://i.postimg.cc/Vky3RzPx/ppc-stacked-logo.jpg' },
     { name: 'MICTSETA', image: 'https://i.postimg.cc/fLKz4JDd/mict-logo.png' },
-    { name: 'Shoprite', image: 'https://i.postimg.cc/1tzG1Zss/a7ba129777f86a6274ee6b336cb3a469.png' },
+    { name: 'Shoprite', image: 'https://i.postimg.cc/QtQXK2Dg/147-imgupscaler-ai-Beta-2K.jpg' },
     { name: 'INHANCE', image: 'https://i.postimg.cc/fLZHtbmZ/inhancelogo.jpg' },
     { name: 'The Gauteng Province', image: 'https://i.postimg.cc/c4WzgX7C/Government-of-Gauteng-logo-svg.png' },
     { name: 'AWQAF SA', image: 'https://i.postimg.cc/jdqbVgJ6/awqaflogo.jpg' },
